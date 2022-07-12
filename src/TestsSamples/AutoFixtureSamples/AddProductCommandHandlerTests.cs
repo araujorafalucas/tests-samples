dd38b9cd67d9ee816114c3ae901e60f118fb9b5e
Lucas Araujo <lucas.rafael.araujo@hotmail.com>
2022-07-11 21:46:28 -0300

    Get Started implement samples with AutoFixture

---
+++ src/TestsSamples/AutoFixtureSamples/AddProductCommandHandlerTests.cs
@@ -0,0 +1,46 @@
+using AutoFixture.Xunit2;
+using Domain.CommandHandlers;
+using Domain.Entities;
+using Domain.Repositories;
+using NSubstitute;
+using Xunit;
+using FluentAssertions;
+using AutoFixture;
+
+namespace AutoFixtureSamples
+{
+    public class AddProductCommandHandlerTests
+    {
+        /// <summary>
+        /// With AutoFixture.Xunit2
+        /// </summary>
+        /// <param name="product"></param>
+        [Theory, AutoData]
+        public void ValidProduct_AddIsCalled_ReturnsTrue_AutoData(Product product)
+        {
+            
+            var productRepository = Substitute.For<IProductsRepository>();
+
+            var addProductCommandHandler = new AddProductCommandHandler(productRepository);
+
+            var result = addProductCommandHandler.Handle(product);
+
+            result.Sucess.Should().BeTrue();
+        }
+
+
+        [Fact]
+        public void ValidProduct_AddIsCalled_ReturnsTrue()
+        {
+            var product = new Fixture().Create<Product>();
+
+            var productRepository = Substitute.For<IProductsRepository>();
+
+            var addProductCommandHandler = new AddProductCommandHandler(productRepository);
+
+            var result = addProductCommandHandler.Handle(product);
+
+            result.Sucess.Should().BeTrue();
+        }
+    }
+}
